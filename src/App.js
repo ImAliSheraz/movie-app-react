e30b02448e30a105d9d8756cde1923d71ada9cd9
Ali Sheraz <ali.sheraz@novatoresols.com>
2022-08-13 12:00:00 +0500

    📦 NEW: CSS and Layout design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
+import './App.css';
+import SearchIcon from './serach.svg';
+
+const API_BASE_URL = 'https://www.omdbapi.com/?apikey=6f443b3a'
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_BASE_URL}&s=${title}`);
+    const movieData = await response.json();
+    console.log(movieData.Search)
+}
 
 const App = () => {
+
+    useEffect(() => {
+        searchMovies('spiderman')
+    }, [])
     return (
         <h1>Hello React</h1>
     )
